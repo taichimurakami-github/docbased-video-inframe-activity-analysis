8f975e75fdce8c5a435e547623185e5e044e0a66
Taichi Murakami <taichi.murakami.s5@dc.tohoku.ac.jp>
2023-06-19 21:53:56 +0900

    Temp: Trying to crip diff area as bounding box

--- src/main.py
+++ src/main.py
@@ -1,104 +1,170 @@
 import os
 import numpy as np
+import json
 from pprint import pprint
+from sklearn.cluster import KMeans
 import cv2
 from PIL import Image
 from utils.CV2VideoUtil import CV2VideoUtil
 from utils.CV2ImageUtil import CV2ImageUtil
+from analyzer.image import (
+    detect_imdiff_by_bgrvalue,
+    detect_imdiff_by_saturation,
+    generate_allzero_uint8_nparr,
+)
 
 data_base_dir = os.path.join(os.path.dirname(__file__), ".data")
 video_file_path = os.path.join(data_base_dir, "EdanMeyerVpt.mp4")
 
 
 def get_video_frame_every_sec_from_src(src: str):
-  video = CV2VideoUtil()
-  cap = video.load(src)
-  _, result_img =  video.get_video_frame_every_sec(cap, 6)
-  return result_img
+    video = CV2VideoUtil()
+    cap = video.load(src)
+    _, result_img = video.get_video_frame_every_sec(cap, 6)
+    return result_img
 
-def generate_allzero_uint8_nparr(width: int, height: int):
-  result = []
-  for i_col in range(height):
-    px_col = []
-    for i_row in range(width):
-      px_col.append([0,0,0])
 
-    result.append(px_col)
+def detect_and_draw_contours_by_kmeans(
+    img_background: cv2.Mat, img_filtered: cv2.Mat
+):
+    fgbg = cv2.createBackgroundSubtractorMOG2()
+    img_fgmask = fgbg.apply(img_background)
+    img_fgmask = fgbg.apply(img_filtered)
 
-  return np.array(result, dtype="uint8")
+    # 検出した矩形領域でクリッピング(クラスタリングを行い，その結果からオブジェクト領域を検出する)
+    N_CLUSTERS = 6
+    Y, X = np.where(img_fgmask > 200)
+    kmeans = KMeans(n_clusters=N_CLUSTERS, random_state=0).fit_predict(
+        np.array([X, Y]).T
+    )
+
+    def get_x_y_limit(Y, X, result, cluster):
+        NO = np.where(result == cluster)
+        x_max = np.max(X[NO])
+        x_min = np.min(X[NO])
+        y_max = np.max(Y[NO])
+        y_min = np.min(Y[NO])
+
+        x_max = int(x_max)
+        x_min = int(x_min)
+        y_max = int(y_max)
+        y_min = int(y_min)
+        return x_min, y_min, x_max, y_max
+
+    def bounding_box(img, x_min, y_min, x_max, y_max):
+        img = cv2.rectangle(
+            img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 5
+        )
+        return img
+
+    # bounding boxを描画
+    for i in range(0, N_CLUSTERS):
+        x_min, y_min, x_max, y_max = get_x_y_limit(Y, X, kmeans, i)
+        img_filtered = bounding_box(img_filtered, x_min, y_min, x_max, y_max)
+
+    return img_filtered
 
 
 def compare_2_imgs(img1src, img2src):
-  image = CV2ImageUtil()
+    image = CV2ImageUtil()
 
-  # imgの構造
-  # [ <- len = 1080(num of pixels of column)
-  #  [
-  #    [255,255,255]
-  #    [255,255,255]
-  #    ...
-  #    [255,255,255]
-  #  ], <- len = 1920(num of pixels of row)
-  #  [
-  #    [255,255,255]
-  #    [255,255,255]
-  #    ...
-  #    [255,255,255]
-  #  ], <- len = 1920(num of pixels of row)
-  #  ...
-  #  [
-  #    [255,255,255]
-  #    [255,255,255]
-  #    ...
-  #    [255,255,255]
-  #  ], <- len = 1920(num of pixels of row)
-  # ]
+    # imgの構造
+    # [ <- len = 1080(num of pixels of column)
+    #  [
+    #    [255,255,255] <- depends on colorspace (ex: [b,g,r], [h,s,v])
+    #    [255,255,255]
+    #    ...
+    #    [255,255,255]
+    #  ], <- len = 1920(num of pixels of row)
+    #  [
+    #    [255,255,255]
+    #    [255,255,255]
+    #    ...
+    #    [255,255,255]
+    #  ], <- len = 1920(num of pixels of row)
+    #  ...
+    #  [
+    #    [255,255,255]
+    #    [255,255,255]
+    #    ...
+    #    [255,255,255]
+    #  ], <- len = 1920(num of pixels of row)
+    # ]
 
-  img1_bgr = image.load(img1src)
-  img2_bgr = image.load(img2src)
+    img1_bgr = image.load(img1src)
+    img2_bgr = image.load(img2src)
 
-  
-  img1 = image.apply_bgr2hsv(img1_bgr)
-  img2 = image.apply_bgr2hsv(img2_bgr)
+    img1_hsv = image.apply_bgr2hsv(img1_bgr)
+    img2_hsv = image.apply_bgr2hsv(img2_bgr)
 
-  # img1 = image.apply__filter_gaussian_blur(image.apply_clahe(image.apply_bgr2gray(img1)))
-  # img2 = image.apply__filter_gaussian_blur(image.apply_clahe(image.apply_bgr2gray(img2)))
+    # img1 = image.apply__filter_gaussian_blur(image.apply_clahe(image.apply_bgr2gray(img1)))
+    # img2 = image.apply__filter_gaussian_blur(image.apply_clahe(image.apply_bgr2gray(img2)))
 
-  # result = image.get_absdiff(img1, img2)
-  # image.show(result, "absdiff_example")
-  # return
+    # 1. 単純な画素差分検出
+    # result = image.get_absdiff(img1_bgr, img2_bgr)
+    # image.show(result, "absdiff_example")
 
-  DIFF_D_MANHATTAN_THRESHOLD = 100.0 # 差分と検出する閾値
-  result = generate_allzero_uint8_nparr(1920, 1080)
+    # 2. bgrベースの差分検出(with nose filter)
+    # (result_diff_img_bgr, result_details) = detect_imdiff_by_bgrvalue(
+    #     img1_bgr, img2_bgr
+    # )
+    # image.show(result_diff_img_bgr, "result_bgrpxbased_example")
 
-  for i_col in range(img1.__len__()):
-    img1row = img1[i_col]
-    img2row = img2[i_col]
+    # 3. hsvベースの差分検出
+    (result_diff_img_hsv, result_details) = detect_imdiff_by_saturation(
+        img1_hsv, img2_hsv
+    )
+    # image.show(
+    #     image.apply_hsv2bgr(result_diff_img_hsv),
+    #     "result_saturationbased_example",
+    # )
+    print(
+        f"img len : {result_diff_img_hsv.__len__()} x {result_diff_img_hsv[0].__len__()}"
+    )
+    print(f"details len : {result_details.__len__()}")
 
-    for i_row in range(img1row.__len__()):
-      img1px = img1row[i_row]
-      img2px = img2row[i_row]
+    # 切り出した画像のうち，矩形領域でない部分を検出
+    # img_background = generate_allzero_uint8_nparr(1920, 1080)
+    # img_filtered = image.apply_filter_gaussian_blur(
+    #     image.apply_hsv2bgr(result_diff_img_hsv), (5, 5), 0
+    # )
+    # image.show(img_filtered, "cropping_test_result")
 
-      print(img1px, img2px)
-      diff_d_manhattan = np.linalg.norm(img1px - img2px, ord=1)
+    tmp_path = os.path.join(os.path.dirname(__file__), ".data", "_temp.png")
+    image.save(
+        image.apply_hsv2bgr(result_diff_img_hsv),
+        tmp_path,
+    )
 
-      if diff_d_manhattan > DIFF_D_MANHATTAN_THRESHOLD:
+    # return
 
-        # 差分が白だったら消す
-        # if img2px[0] < 240 and img2px[1] < 240 and img2px[2] < 240:
-          result[i_col, i_row] = img2px
+    img_base = image.apply_bgr2gray(
+        image.apply_filter_gaussian_blur(image.load(tmp_path), (5, 5), 0)
+    )
 
+    contours, hierarchy = cv2.findContours(
+        img_base,
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_NONE,
+    )
 
-        # result.append([
-        #   i_row, # position of x
-        #   i_col, # position of y
-        #   diff_d_manhattan, # size of diff
-        #   img1px, # color mapping of x
-        #   img2px
-        #   ])
+    img_disp = image.apply_gray2bgr(img_base)
 
-  # pprint(result)
-  image.show(result,"result")
+    cv2.drawContours(
+        image=img_disp,
+        contours=contours,
+        contourIdx=-1,
+        color=(0, 0, 255),
+        thickness=4,  # -1を指定すると塗りつぶしになる
+        hierarchy=hierarchy,
+    )
+
+    # for contour in contours:
+    #     for point in contour:
+    #         cv2.circle(img_disp, point[0], 3, (0, 255, 0), -1)
+
+    image.show(img_disp, "cropping_test_result")
+
 
 # image = CV2ImageUtil()
 # j = 0
@@ -107,10 +173,15 @@ def compare_2_imgs(img1src, img2src):
 #   # current_frame_gray = image.apply_grayscale(result_img[i+1])
 
 #   image.save(result_img[i], os.path.join(data_base_dir, f"frame_"))
-  
-#   j = j+1
-#   if j> 3:
+
+#   j = j + 1
+#   if j > 3:
 #     return
 
 
-compare_2_imgs(os.path.join(data_base_dir,"66.jpg"), os.path.join(data_base_dir,"67.jpg"))
+compare_2_imgs(
+    # os.path.join(data_base_dir, "66.jpg"),
+    # os.path.join(data_base_dir, "67.jpg"),
+    os.path.join(data_base_dir, "149.jpg"),
+    os.path.join(data_base_dir, "150.jpg"),
+)
--- src/utils/CV2ImageUtil.py
+++ src/utils/CV2ImageUtil.py
@@ -1,39 +1,43 @@
 import cv2
 
+
 class CV2ImageUtil:
+    def load(self, file_path):
+        return cv2.imread(file_path)
 
-  def load(self, file_path):
-    return cv2.imread(file_path)
-  
-  def apply_bgr2gray(self, cv2img:cv2.Mat):
-    return cv2.cvtColor(cv2img, cv2.COLOR_BGR2GRAY)
+    def apply_bgr2gray(self, cv2img: cv2.Mat):
+        return cv2.cvtColor(cv2img, cv2.COLOR_BGR2GRAY)
 
-  def apply_gray2bgr(self, cv2img:cv2.Mat):
-    return cv2.cvtColor(cv2img, cv2.COLOR_GRAY2BGR)
+    def apply_gray2bgr(self, cv2img: cv2.Mat):
+        return cv2.cvtColor(cv2img, cv2.COLOR_GRAY2BGR)
 
-  def apply_bgr2hsv(self, cv2img:cv2.Mat):
-    return cv2.cvtColor(cv2img, cv2.COLOR_BGR2HSV)
+    def apply_bgr2hsv(self, cv2img: cv2.Mat):
+        return cv2.cvtColor(cv2img, cv2.COLOR_BGR2HSV)
 
-  def apply_hsv2bgr(self, cv2img:cv2.Mat):
-    return cv2.cvtColor(cv2img, cv2.COLOR_HSV2BGR)
+    def apply_hsv2bgr(self, cv2img: cv2.Mat):
+        return cv2.cvtColor(cv2img, cv2.COLOR_HSV2BGR)
 
-  def apply_bgr2bin(self, cv2img:cv2.Mat):
-    return cv2.threshold(cv2img, 0, 255, cv2.THRESH_OTSU)
+    def apply_bgr2bin(self, cv2img: cv2.Mat):
+        return cv2.threshold(cv2img, 0, 255, cv2.THRESH_OTSU)
 
-  def apply__filter_gaussian_blur(self, cv2img: cv2.Mat, ksize=(13,13), sigmaX=0):
-    return cv2.GaussianBlur(cv2img, ksize, sigmaX)
+    def apply_filter_gaussian_blur(
+        self, cv2img: cv2.Mat, ksize=(13, 13), sigmaX=0
+    ):
+        return cv2.GaussianBlur(cv2img, ksize, sigmaX)
 
-  def apply_clahe(self, cv2img:cv2.Mat, clipLimit=30.0, tileGridSize=(10,10)):
-    clahe = cv2.createCLAHE(clipLimit, tileGridSize)
-    return clahe.apply(cv2img)
+    def apply_clahe(
+        self, cv2img: cv2.Mat, clipLimit=30.0, tileGridSize=(10, 10)
+    ):
+        clahe = cv2.createCLAHE(clipLimit, tileGridSize)
+        return clahe.apply(cv2img)
 
-  def get_absdiff(self, cv2img1: cv2.Mat, cv2img2: cv2.Mat):
-    return cv2.absdiff(cv2img1, cv2img2)
+    def get_absdiff(self, cv2img1: cv2.Mat, cv2img2: cv2.Mat):
+        return cv2.absdiff(cv2img1, cv2img2)
 
-  def show(self, cv2img: cv2.Mat, window_name:str):
-    cv2.imshow(window_name,cv2img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    def show(self, cv2img: cv2.Mat, window_name: str):
+        cv2.imshow(window_name, cv2img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-  def save(self, cv2img: cv2.Mat, file_path:str):
-    cv2.imwrite(file_path, cv2img)
+    def save(self, cv2img: cv2.Mat, file_path: str):
+        cv2.imwrite(file_path, cv2img)
