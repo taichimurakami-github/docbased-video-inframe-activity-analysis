6638ac799eca4edef76c460a05cb3f17a76d3b78
Taichi Murakami <taichi.murakami.s5@dc.tohoku.ac.jp>
2023-06-27 22:41:28 +0900

    Make queue private var, Add delete_lapdata option

--- src/utils/Benchmark.py
+++ src/utils/Benchmark.py
@@ -2,26 +2,29 @@ from datetime import datetime
 
 
 class Benchmark:
-    # private _benchmark_laptime_queue =
+    # private __benchmark_laptime_queue =
     # format -> [key: str] : [(Done or Undone), startTime: datetime, endTime: datetime, diff: timedelta]
     # {
     #     "_lap_id_01_": [True, 23493939, 23498323, 4384],
     #     "_lap_id_02_": [False, 23593821],
     #     ...
     # }
-    _benchmark_laptime_queue = {}
+    __benchmark_laptime_queue = {}
 
     def start_benchmark_lap(self, lap_id: str):
-        if lap_id in self._benchmark_laptime_queue.keys():
+        if lap_id in self.__benchmark_laptime_queue.keys():
             raise Exception(f"ERROR: Duplicated lap_id({lap_id}) detected")
-        self._benchmark_laptime_queue[lap_id] = [False, datetime.now()]
+        self.__benchmark_laptime_queue[lap_id] = [False, datetime.now()]
 
     def end_benchmark_lap(self, lap_id: str):
-        self._benchmark_laptime_queue[lap_id].append(datetime.now())
-        self._benchmark_laptime_queue[lap_id][0] = True  # set status to Done
+        self.__benchmark_laptime_queue[lap_id].append(datetime.now())
+        self.__benchmark_laptime_queue[lap_id][0] = True  # set status to Done
 
-    def show_benchmark_results(self):
-        for lap_id, laptime_data in self._benchmark_laptime_queue.items():
+    def delete_lapdata(self):
+        self.__benchmark_laptime_queue = {}
+
+    def show_benchmark_results(self, delete_lapdata=False):
+        for lap_id, laptime_data in self.__benchmark_laptime_queue.items():
             status = laptime_data[0]
             t_start = laptime_data[1]
             t_end = None
@@ -38,3 +41,6 @@ class Benchmark:
                 print(
                     f"[{lap_id}] start at {t_start} , This lap has never finished"
                 )
+
+        if delete_lapdata:
+            self.delete_lapdata()
